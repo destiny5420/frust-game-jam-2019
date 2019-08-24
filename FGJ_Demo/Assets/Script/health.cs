b8e9b00535b1c9c9273bc26a661c22ca45525ab4
sihweichen <s910941@gmail.com>
2019-08-24 18:53:40 +0800

    update

--- FGJ_Demo/Assets/Script/health.cs
+++ FGJ_Demo/Assets/Script/health.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
 	public const int maxHealth = 100;
+
+	[SyncVar(hook = "OnChangeHealth")]
 	public int currentHealth = maxHealth;
+
 	public RectTransform healthBar;
 
 	public void TakeDamage(int amount)
 	{
+		if (!isServer)
+		{
+			return;
+		}
+		Debug.Log("TakeDamage :" + amount);
 		currentHealth -= amount;
 		if (currentHealth <= 0)
 		{
 			currentHealth = 0;
-			Debug.Log("Dead!");
+			Debug.LogError("Dead!!!");
 		}
+	}
 
+	void OnChangeHealth(int currentHealth)
+	{
 		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
 	}
 }
