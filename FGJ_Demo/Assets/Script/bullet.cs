f5c403c97705092facf424efe6b8041c8badb94f
sihweichen <s910941@gmail.com>
2019-08-24 15:46:03 +0800

    update

---
+++ FGJ_Demo/Assets/Script/bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject hit = collision.gameObject;
+		health hp = hit.GetComponent<health>();
+		if (hp != null)
+		{
+			hp.TakeDamage(10);
+		}
+
+		Destroy(gameObject);
+	}
+}
