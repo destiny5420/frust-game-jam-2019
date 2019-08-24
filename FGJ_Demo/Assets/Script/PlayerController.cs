2b9b348319420966edbf9639f7b9d85d3e4aecde
sihweichen <s910941@gmail.com>
2019-08-24 15:04:13 +0800

    update

--- FGJ_Demo/Assets/Script/PlayerController.cs
+++ FGJ_Demo/Assets/Script/PlayerController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,10 +18,75 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
 	{
+		switch (netId.Value)
+		{
+			case 1:
+				GetComponent<MeshRenderer>().material.color = Color.blue;
+				break;
+			case 2:
+				GetComponent<MeshRenderer>().material.color = Color.red;
+				break;
+			case 3:
+				GetComponent<MeshRenderer>().material.color = Color.green;
+				break;
+			case 4:
+				GetComponent<MeshRenderer>().material.color = Color.white;
+				break;
+		}
+
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+
+		Debug.Log("Net ID:" + netId);
 		float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+	}
+
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		GameObject bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		// self
+		this.transform.position += (Vector3.forward * netId.Value);
+	}
+
+	[Command]
+	void CmdFire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		GameObject bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Spawn the bullet on the Clients
+		NetworkServer.Spawn(bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
 	}
 }
