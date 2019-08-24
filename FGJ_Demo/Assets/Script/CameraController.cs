ff80c1df9129fe94257b210bd6b20bf115caf4a2
Paper <destiny5420@gmail.com>
2019-08-24 16:09:18 +0800

    [Add] 新增攝影機抓Player

---
+++ FGJ_Demo/Assets/Script/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] PlayerController m_clsPlayerController;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        FindPlayerTarget();
+    }
+
+    void FindPlayerTarget()
+    {
+        if (m_clsPlayerController != null)
+            return;
+
+        object[] arrayObj = GameObject.FindObjectsOfType<PlayerController>();
+        
+        for (int i = 0; i < arrayObj.Length; i++)
+        {
+            NetworkBehaviour player = arrayObj[i] as NetworkBehaviour;
+            if (player.isLocalPlayer == true)
+            {
+                m_clsPlayerController = (PlayerController)player;
+                break;
+            }
+        }
+    }
+}
