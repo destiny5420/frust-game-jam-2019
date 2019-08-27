3fb9d9618bedbdf038489137869ee758c757fc0e
purfate <purfate@users.noreply.github.com>
2019-08-27 23:09:43 +0800

    update Uicanvas

--- FGJ_Demo/Assets/Script/UiState.cs
+++ FGJ_Demo/Assets/Script/UiState.cs
@@ -8,32 +8,27 @@ public class UiState : MonoBehaviour
     int Uistate_old ;
     public GameObject[] UI;
 
-
-
-    private void Start()
+    public void ButtonDown(int aa)
     {
-               
+        Uistate_old = Uistate; Uistate = aa;
     }
     void GetButton()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { Uistate_old = Uistate; Uistate = 1; }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { Uistate_old = Uistate; Uistate = 1; }//正常順位
         if (Input.GetKeyDown(KeyCode.Alpha2)) { Uistate_old = Uistate; Uistate = 2; }
         if (Input.GetKeyDown(KeyCode.Alpha3)) { Uistate_old = Uistate; Uistate = 3; }
         if (Input.GetKeyDown(KeyCode.Alpha4)) { Uistate_old = Uistate; Uistate = 4; }
         if (Input.GetKeyDown(KeyCode.Alpha5)) { Uistate_old = Uistate; Uistate = 5; }
         if (Input.GetKeyDown(KeyCode.Alpha6)) { Uistate_old = Uistate; Uistate = 6; }
 
-        if (Uistate_old != Uistate) {
+        if (Uistate_old != Uistate) {    //-1 > 0~5
             UI[Uistate_old-1].SetActive(false);
             UI[Uistate-1].SetActive(true);
-
-
+            Uistate_old = Uistate ;
         }
 
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         GetButton();
     }
