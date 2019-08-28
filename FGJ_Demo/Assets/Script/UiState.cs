70181d3b80cc81626d47dc6ce3af8d1ff62ce238
purfate <purfate@users.noreply.github.com>
2019-08-28 23:02:15 +0800

    魔改

--- FGJ_Demo/Assets/Script/UiState.cs
+++ FGJ_Demo/Assets/Script/UiState.cs
@@ -8,21 +8,21 @@ public class UiState : MonoBehaviour
     int Uistate_old ;
     public GameObject[] UI;
 
-    public void ButtonDown(int aa)
+    public void ButtonDown(int aa) 
     {
-        Uistate_old = Uistate; Uistate = aa;
+        Uistate_old = Uistate; Uistate = aa; // GetButton();    //有Bug 超出範圍
     }
     void GetButton()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { Uistate_old = Uistate; Uistate = 1; }//正常順位
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { Uistate_old = Uistate; Uistate = 1; }
         if (Input.GetKeyDown(KeyCode.Alpha2)) { Uistate_old = Uistate; Uistate = 2; }
         if (Input.GetKeyDown(KeyCode.Alpha3)) { Uistate_old = Uistate; Uistate = 3; }
         if (Input.GetKeyDown(KeyCode.Alpha4)) { Uistate_old = Uistate; Uistate = 4; }
         if (Input.GetKeyDown(KeyCode.Alpha5)) { Uistate_old = Uistate; Uistate = 5; }
         if (Input.GetKeyDown(KeyCode.Alpha6)) { Uistate_old = Uistate; Uistate = 6; }
 
-        if (Uistate_old != Uistate) {    //-1 > 0~5
-            UI[Uistate_old-1].SetActive(false);
+        if (Uistate_old != Uistate) {  
+            if (Uistate_old - 1 != 0)  UI[Uistate_old - 1].SetActive(false); 
             UI[Uistate-1].SetActive(true);
             Uistate_old = Uistate ;
         }
