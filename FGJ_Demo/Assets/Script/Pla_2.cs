bf8f964c6fa6725bb90b5db5ee7be1fb861b2787
purfate <purfate@outlook.com>
2020-02-18 13:25:27 +0800

    s

--- FGJ_Demo/Assets/Script/Pla_2.cs
+++ FGJ_Demo/Assets/Script/Pla_2.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class Pla_2 : MonoBehaviour ///顯示狀態
 {
     
-    public GameObject 功能二;
-    bool aaaaa =false;
-    public int 遊戲狀態=0; // 0未進入 1Enter
+    public GameObject Canvas_UI; 
+    bool GameStart =false;
+    public int 遊戲狀態=0; // 0 未進入 1Enter
     void Awake()
     {
-        if (!功能二)//抓燈光 
+        if (!Canvas_UI)//抓燈光 
         {
-            GameObject.Find("Directional Light").GetComponent<Pla_2>().aaaaa = true;
+            GameObject.Find("Directional Light").GetComponent<Pla_2>().GameStart = true;
         }
-    }
-
-    // Update is called once per frame
+    } 
     void Update()
     {
-        if (aaaaa) { aaaaa = false; 功能二.SetActive(true); //燈光開Canvas
+        if (GameStart) { GameStart = false; Canvas_UI.SetActive(true); //用燈光 開 UI 
             遊戲狀態 = 1;
             GameObject.Find("slot1").GetComponent<Slot>().OpenGame = true;
             GameObject.Find("slot2").GetComponent<Slot>().OpenGame = true;
